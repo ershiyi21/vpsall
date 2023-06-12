4097b99cc5f6f7791c1d4c4e4897792ea8d672fe
ershiyi21 <68776500+ershiyi21@users.noreply.github.com>
2023-06-12 21:37:44 +0800

    Update embybetter.sh

--- emby/embybetter.sh
+++ emby/embybetter.sh
@@ -15,43 +15,49 @@ cd /app/emby/dashboard-ui
 [[ -f index.html.cp ]] && rm -rf index.html && cp index.html.cp index.html
 
 # 创建emby-crx目录并下载所需文件
-echo "开始安装crx-emby首页大屏海报展示..."
-rm -rf emby-crx
-mkdir -p emby-crx
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/css/style.css -P emby-crx/
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/common-utils.js -P emby-crx/
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/jquery-3.6.0.min.js -P emby-crx/
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/md5.min.js -P emby-crx/
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/content/main.js -P emby-crx/
+echo "开始安装首页大屏海报展示..."
+[[ -f PhotoCarousel.js ]] && rm -rf PhotoCarousel.js
+wget https://raw.githubusercontent.com/ershiyi21/vpsall/master/emby/PhotoCarousel.js
+sed -i '/<\/body>/i\<script src="./PhotoCarousel.js"></script>' index.html
+echo "首页大屏海报展示安装完成！"
+
+#rm -rf emby-crx
+#mkdir -p emby-crx
+#wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/css/style.css -P emby-crx/
+#wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/common-utils.js -P emby-crx/
+#wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/jquery-3.6.0.min.js -P emby-crx/
+#wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/md5.min.js -P emby-crx/
+#wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/content/main.js -P emby-crx/
 
 # 读取index.html文件内容
-content=$(cat index.html)
+#content=$(cat index.html)
 
 # 检查index.html是否包含emby-crx
-if [[ "$content" == *"emby-crx"* ]]; then
-    echo "Index.html already contains emby-crx, skipping insertion."
-else
-    # 定义要插入的代码
-    code='<link rel="stylesheet" id="theme-css" href="emby-crx/style.css" type="text/css" media="all" />\n<script src="emby-crx/common-utils.js"></script>\n<script src="emby-crx/jquery-3.6.0.min.js"></script>\n<script src="emby-crx/md5.min.js"></script>\n<script src="emby-crx/main.js"></script>'
-
-    # 在</head>之前插入代码
-    new_content=$(echo -e "${content/<\/head>/$code<\/head>}")
-
-    # 将新内容写入index.html文件
-    echo -e "$new_content" > index.html
-    
-    echo "crx-emby安装完成！"
-fi
+#if [[ "$content" == *"emby-crx"* ]]; then
+#    echo "Index.html already contains emby-crx, skipping insertion."
+#else
+#    # 定义要插入的代码
+#    code='<link rel="stylesheet" id="theme-css" href="emby-crx/style.css" type="text/css" media="all" />\n<script src="emby-crx/common-utils.js"></script>\n<script src="emby-crx/jquery-3.6.0.min.js"></script>\n<script src="emby-crx/md5.min.js"></script>\n<script src="emby-crx/main.js"></script>'
+#
+#    # 在</head>之前插入代码
+#    new_content=$(echo -e "${content/<\/head>/$code<\/head>}")
+#
+#    # 将新内容写入index.html文件
+#    echo -e "$new_content" > index.html
+#    
+#    echo "crx-emby安装完成！"
+#fi
 
 #安装网页版本调用第三方播放器插件
+echo "开始安装第三方播放器插件..."
 rm -rf embyLaunchPotplayer.js
 wget https://raw.githubusercontent.com/bpking1/embyExternalUrl/master/embyWebAddExternalUrl/embyLaunchPotplayer.js
-sed -i '/<\/body>/i\<script type="text/javascript" src="./embyLaunchPotplayer.js"></script>' index.html
+sed -i '/<\/body>/i\<script src="./embyLaunchPotplayer.js"></script>' index.html
 echo "第三方播放器插件安装完成！"
 
 
 #emby 4.7.12.0 mod
-read -p "是否开启emby 4.7.12.0 mod模式，服务端验证已经开启白名单，故默认否 (y/n)? " confirm
+read -p "是否开启 emby 4.7.12.0 mod 模式，服务端验证已经开启白名单，故默认否 (y/n)? " confirm
 
 # 如果确认信息为"y"或"Y"，则mod
 if [[ $confirm == [yY] ]]; then
