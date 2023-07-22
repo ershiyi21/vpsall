3314a0b0d8851b88acae5a68b6357afc2028102e
ershiyi21 <68776500+ershiyi21@users.noreply.github.com>
2023-07-22 12:57:34 +0800

    Update aria2.sh

--- aria2.sh
+++ aria2.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-mkdir -p /opt/aria2/temp && cd /opt/aria2/temp
+mkdir -p /opt/aria2/temp 
+cd /opt/aria2/temp
 wget https://github.com/P3TERX/Aria2-Pro-Core/releases/download/1.36.0_2021.08.22/aria2-1.36.0-static-linux-amd64.tar.gz
 tar -zxvf aria2-1.36.0-static-linux-amd64.tar.gz
 mv aria2c /usr/local/bin
 rm -rf /opt/aria2/temp
-mkdir /opt/aria2/config && mkdir /opt/aria2/downloads && touch /opt/aria2/config/aria2.session
+mkdir /opt/aria2/config 
+mkdir /opt/aria2/downloads 
+touch /opt/aria2/config/aria2.session
 
 wget -P /opt/aria2/config https://raw.githubusercontent.com/ershiyi21/vpsall/main/file/aria2.conf
 
