fc330bcf49a68d963ddf7a36aa5a539d699a252d
ershiyi21 <68776500+ershiyi21@users.noreply.github.com>
2023-07-30 16:15:00 +0800

    Update sshblock.sh

--- sshblock.sh
+++ sshblock.sh
@@ -4,11 +4,12 @@ if [[ $? == 0 ]],then
 sudo apt-get update
 sudo apt-get install fail2ban
 fi
+##连续10次错误，封禁10小时
 echo "[sshd]
 enabled = true
 port = 22
 maxretry = 10
 findtime = 600
-bantime = 3600" > /etc/fail2ban/jail.local
+bantime = 36000" > /etc/fail2ban/jail.local
 sudo systemctl restart fail2ban
 sudo systemctl enable fail2ban
