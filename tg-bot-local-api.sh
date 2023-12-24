ed9064696c558cf34e3071f24c16d8f8e7c55309
ershiyi21 <68776500+ershiyi21@users.noreply.github.com>
2023-12-24 14:00:37 +0800

    Create tg-bot-local-api.sh

---
+++ tg-bot-local-api.sh
@@ -0,0 +1,182 @@
+#!/bin/bash
+
+COMPOSE_FILE="/home/tg-local-api/docker-compose.yml"
+DATA_DIR="/home/tg-local-api/data"
+
+# 检测容器是否已安装
+check_containers_installed() {
+  local containers_installed=0
+  
+  if docker-compose -f "$COMPOSE_FILE" ps -q | grep -q -w "telegram-bot-api"; then
+    containers_installed=$((containers_installed+1))
+  fi
+  
+  if docker-compose -f "$COMPOSE_FILE" ps -q | grep -q -w "nginx"; then
+    containers_installed=$((containers_installed+1))
+  fi
+  
+  echo $containers_installed
+}
+
+# 创建目录
+create_directory() {
+  if [[ ! -d "$1" ]]; then
+    mkdir -p "$1"
+  fi
+}
+
+# 创建 telegram-bot-api-data 卷
+create_telegram_bot_api_data_volume() {
+  docker volume create telegram-bot-api-data
+}
+
+# 提示用户输入参数
+prompt_user_input() {
+  read -p "请输入 Telegram API ID: " telegram_api_id
+  read -p "请输入 Telegram API Hash: " telegram_api_hash
+  
+  read -p "请输入 Telegram Bot API 外部访问端口 (默认为 28081): " telegram_bot_api_port1
+  telegram_bot_api_port1=${telegram_bot_api_port1:-28082}
+  
+  read -p "是否映射 Telegram Bot API 外部统计端口 (默认为否)? [Y/n]: " map_telegram_bot_api_port2
+  map_telegram_bot_api_port2=${map_telegram_bot_api_port2:-n}
+  
+  if [[ $map_telegram_bot_api_port2 =~ ^[Yy]$ ]]; then
+    read -p "请输入 Telegram Bot API 外部统计端口 (默认为 28082): " telegram_bot_api_port2
+    telegram_bot_api_port2=${telegram_bot_api_port2:-28082}
+  fi
+  
+  read -p "请输入 Nginx 外部访问端口 (默认为 28080): " nginx_port
+  nginx_port=${nginx_port:-28080}
+}
+
+# 检查输入的有效性
+validate_input() {
+  if [[ -z $telegram_api_id || -z $telegram_api_hash ]]; then
+    echo "Telegram API ID 和 Telegram API Hash 不能为空"
+    return 1
+  fi
+  
+  if ! [[ $telegram_bot_api_port1 =~ ^[0-9]+$ ]]; then
+    echo "Telegram Bot API 外部访问端口必须是一个有效的整数"
+    return 1
+  fi
+  
+  if ! [[ $telegram_bot_api_port2 =~ ^[0-9]+$ ]]; then
+    echo "Telegram Bot API 外部统计端口必须是一个有效的整数"
+    return 1
+  fi
+  
+  if ! [[ $nginx_port =~ ^[0-9]+$ ]]; then
+    echo "Nginx 外部端口必须是一个有效的整数"
+    return 1
+  fi
+}
+
+# 创建并启动容器
+create_and_start_containers() {
+  cat << EOF > "$COMPOSE_FILE"
+version: '3.8'
+
+services:
+  telegram-bot-api:
+    image: aiogram/telegram-bot-api:latest
+    environment:
+      TELEGRAM_API_ID: "$telegram_api_id"
+      TELEGRAM_API_HASH: "$telegram_api_hash"
+      TELEGRAM_LOCAL: Yes
+      TELEGRAM_STAT: Yes
+    volumes:
+      - telegram-bot-api-data:/var/lib/telegram-bot-api
+    ports:
+      - "$telegram_bot_api_port1:8081"
+EOF
+
+  if [[ $map_telegram_bot_api_port2 =~ ^[Yy]$ ]]; then
+    echo "    - \"$telegram_bot_api_port2:8082\"" >> "$COMPOSE_FILE"
+  fi
+
+  cat << EOF >> "$COMPOSE_FILE"
+  restart: unless-stopped
+
+  nginx:
+    image: nginx:latest
+    volumes:
+      - telegram-bot-api-data:/telegram-bot-api-data
+      - ./nginx.conf:/etc/nginx/conf.d/default.conf
+    ports:
+      - "$nginx_port:8080"
+    restart: unless-stopped
+
+volumes:
+  telegram-bot-api-data:
+    external: true
+EOF
+
+  docker-compose -f "$COMPOSE_FILE" up -d
+}
+
+# 安装容器
+install_containers() {
+  local containers_installed=$(check_containers_installed)
+  
+  if [[ $containers_installed -eq 2 ]]; then
+    echo "容器已安装"
+    return
+  fi
+  
+  create_directory "/home/tg-local-api"
+  create_directory "$DATA_DIR"
+  create_telegram_bot_api_data_volume
+  prompt_user_input
+  
+  while ! validate_input; do
+    prompt_user_input
+  done
+  
+  create_and_start_containers
+}
+
+# 停止容器
+stop_containers() {
+  docker-compose -f "$COMPOSE_FILE" down
+}
+
+# 卸载容器
+uninstall_containers() {
+  docker-compose -f "$COMPOSE_FILE" down --volumes
+  docker volume rm telegram-bot-api-data
+}
+
+# 脚本管理菜单
+show_menu() {
+  echo "脚本管理菜单"
+  echo "1. 安装容器"
+  echo "2. 停止容器"
+  echo "3. 卸载容器"
+  echo "4. 退出"
+}
+
+# 处理用户输入
+handle_input() {
+  read -p "请输入选项：" option
+  case $option in
+    1) install_containers ;;
+    2) stop_containers ;;
+    3) uninstall_containers ;;
+    4) exit ;;
+    *) echo "无效的选项" ;;
+  esac
+}
+
+# 主函数
+main() {
+  while true; do
+    show_menu
+    handle_input
+    echo
+  done
+}
+
+# 执行主函数
+main
